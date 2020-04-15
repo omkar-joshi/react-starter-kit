f4bb392ee551ee031ed5f5523a3a9455c73a86c9
Dnyaneshwar Bhalerao <dnyaneshwar.bhalerao@velotio.com>
2020-04-15 16:15:20 +0530

    Moved list iterator to seperate function

--- src/containers/Demo/AccordionList.tsx
+++ src/containers/Demo/AccordionList.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Accordion, Button, Card } from 'react-bootstrap';
-
+import { Button, Card } from 'react-bootstrap';
+import Accordion from 'react-bootstrap/Accordion';
 interface IAccordionProps {
   listData: DemoData[];
 }
 
 const AccordionList: React.FC<IAccordionProps> = ({ listData }) => {
-  return (
-    <Accordion>
-      {listData.map(listItem => (
-        <Card key={listItem.id}>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey={`${listItem.id}`}>
-              {listItem.title}
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey={`${listItem.id}`}>
-            <Card.Body>{listItem.body}</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      ))}
-    </Accordion>
-  );
+  const renderList = listData.map(listItem => (
+    <Card key={listItem.id}>
+      <Card.Header>
+        <Accordion.Toggle as={Button} variant="link" eventKey={`${listItem.id}`}>
+          {listItem.title}
+        </Accordion.Toggle>
+      </Card.Header>
+      <Accordion.Collapse eventKey={`${listItem.id}`}>
+        <Card.Body>{listItem.body}</Card.Body>
+      </Accordion.Collapse>
+    </Card>
+  ));
+
+  return <Accordion>{renderList}</Accordion>;
 };
 
 export default AccordionList;
