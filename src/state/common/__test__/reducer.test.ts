33ae17800de3b5f88aa94a4999ea141d4a6e8889
Dnyaneshwar Bhalerao <dnyaneshwar.bhalerao@velotio.com>
2020-05-04 14:40:02 +0530

    Added test cases for reducer

--- src/state/common/__test__/reducer.test.ts
+++ src/state/common/__test__/reducer.test.ts
@@ -1,14 +1,30 @@
 import { SET_LOADING } from 'src/state/common/actions';
+import { GET_DEMO_DATA_SUCCESS } from 'src/state/demo/actions';
+import { LOADING_COMPLETED } from '../actions';
 import reducers from '../reducer';
 
-test('reducers', () => {
-  const initialState: PageLoadingStore = {
-    demoLoading: false,
-  };
-  const mockPayLoadPage = {
-    page: 'somePage',
-  };
-  const actualState = reducers(initialState, { type: SET_LOADING, payload: mockPayLoadPage });
-  const expectedState = { ...initialState, demoLoading: false, [`${mockPayLoadPage.page}Loading`]: true };
-  expect(actualState).toEqual(expectedState);
+describe('Test reducer', () => {
+  it('Test for when action is SET_LOADING', () => {
+    const initialState: PageLoadingStore = {
+      demoLoading: false,
+    };
+    const mockPayLoadPage = {
+      page: 'somePage',
+    };
+    const actualState = reducers(initialState, { type: SET_LOADING, payload: mockPayLoadPage });
+    const expectedState = { ...initialState, demoLoading: false, [`${mockPayLoadPage.page}Loading`]: true };
+    expect(actualState).toEqual(expectedState);
+  });
+
+  it('Test for when action is LOADING_COMPLETED', () => {
+    const initialState: PageLoadingStore = {
+      demoLoading: false,
+    };
+    const mockPayLoadPage = {
+      page: 'somePage',
+    };
+    const actualState = reducers(initialState, { type: LOADING_COMPLETED, payload: mockPayLoadPage });
+    const expectedState = { ...initialState, demoLoading: false, [`${mockPayLoadPage.page}Loading`]: false };
+    expect(actualState).toEqual(expectedState);
+  });
 });
